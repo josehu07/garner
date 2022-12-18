49904dafea44d1c99f9073eedf8b4133685f3488
Guanzhou Hu <guanzhou.hu@wisc.edu>
2022-12-17 23:08:38 -0700

    update benchmark scripts

--- bench/simple_bench.cpp
+++ bench/simple_bench.cpp
@@ -32,6 +32,7 @@ static unsigned NUM_THREADS = 16;
 static size_t NUM_OPS_WARMUP = 50000;
 static size_t MAX_OPS_PER_TXN = 10;
 static unsigned SCAN_PERCENTAGE = 25;
+static unsigned WRITE_PERCENTAGE = 10;
 // TODO: control scan range key space
 
 struct TxnStats {
@@ -48,7 +49,7 @@ static void client_thread_func(std::stop_token stop_token,
                                garner::Garner* gn,
                                const std::vector<std::string>* warmup_keys,
                                TxnStats* stats, std::latch* init_barrier) {
-    *stats = {0, 0, 0., 0., 0., 0.};
+    
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -56,14 +57,18 @@ static void client_thread_func(std::stop_token stop_token,
     std::string val = gen_rand_string(gen, VAL_LEN);
 
     std::uniform_int_distribution<size_t> rand_idx(0, warmup_keys->size() - 1);
-    std::uniform_int_distribution<unsigned> rand_is_scan(1, 100);
+    std::uniform_int_distribution<unsigned> rand_is_scan(
+        1, SCAN_PERCENTAGE * 10 + (100 - SCAN_PERCENTAGE));
 
     // in non-scan transactions, 5% are write operations
-    std::uniform_int_distribution<unsigned> rand_op_type(1, 20);
+    std::uniform_int_distribution<unsigned> rand_op_type(1,
+                                                         100 - SCAN_PERCENTAGE);
 
     auto GenRandomReq = [&](bool is_scan_txn) -> GarnerReq {
         // randomly pick an op type
-        GarnerOp op = is_scan_txn ? SCAN : (rand_op_type(gen) == 1) ? PUT : GET;
+        GarnerOp op = is_scan_txn
+                          ? SCAN
+                          : (rand_op_type(gen) <= WRITE_PERCENTAGE) ? PUT : GET;
 
         if (op == GET) {
             std::string key = warmup_keys->at(rand_idx(gen));
@@ -102,7 +107,7 @@ static void client_thread_func(std::stop_token stop_token,
         if (stop_token.stop_requested()) break;
 
         // generate scan only or not decision
-        bool scan_txn = (rand_is_scan_txn(gen) < SCAN_PERCENTAGE);
+        bool scan_txn = (rand_is_scan_txn(gen) <= SCAN_PERCENTAGE * 10);
 
         // generate number of ops for this transaction
         size_t txn_ops = scan_txn ? rand_txn_ops_scan(gen) : rand_txn_ops(gen);
@@ -159,7 +164,8 @@ static void simple_benchmark_round(garner::TxnProtocol protocol) {
 
     std::cout << " Degree=" << TEST_DEGREE << " #threads=" << NUM_THREADS
               << " length=" << ROUND_SECS << "s"
-              << " scan=" << SCAN_PERCENTAGE << "%" << std::endl;
+              << " scan=" << SCAN_PERCENTAGE << "%"
+              << " write=" << WRITE_PERCENTAGE << "%" << std::endl;
 
     // garner::BPTreeStats stats = gn->GatherStats(true);
     // std::cout << stats << std::endl;
@@ -271,7 +277,9 @@ int main(int argc, char* argv[]) {
         "m,max_ops_txn", "max number of ops per transaction",
         cxxopts::value<size_t>(MAX_OPS_PER_TXN)->default_value("10"))(
         "c,scan_percent", "percentage of scan operations",
-        cxxopts::value<unsigned>(SCAN_PERCENTAGE)->default_value("25"));
+        cxxopts::value<unsigned>(SCAN_PERCENTAGE)->default_value("25"))(
+        "r,write_percent", "percentage of write operations",
+        cxxopts::value<unsigned>(WRITE_PERCENTAGE)->default_value("10"));
     auto result = cmd_args.parse(argc, argv);
 
     std::set<std::string> valid_protocols{"none", "silo", "silo_hv", "silo_nr"};
@@ -302,6 +310,12 @@ int main(int argc, char* argv[]) {
     if (MAX_OPS_PER_TXN < 10) {
         std::cerr << "Error: max number of ops per transaction too small "
                   << MAX_OPS_PER_TXN << std::endl;
+        return 1;
+    }
+
+    if (WRITE_PERCENTAGE + SCAN_PERCENTAGE > 100) {
+        std::cerr << "Error: percent of scan and write ops exceed 100%"
+                  << std::endl;
         return 1;
     }
 
--- scripts/simple_bench.py
+++ scripts/simple_bench.py
@@ -9,12 +9,18 @@ import os
 import matplotlib.pyplot as plt
 
 GARNER_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-PROG_SIMPLE_BENCH = f"{GARNER_DIR}/build/bench/simple_bench"
 
 PROTOCOLS = ("silo", "silo_hv", "silo_nr")
 
 
-def run_simple_benchmarks(scan_percentages, output_prefix, num_threads):
+def simple_bench_path(collect_latency):
+    build_dir = "build-stats" if collect_latency else "build"
+    return f"{GARNER_DIR}/{build_dir}/bench/simple_bench"
+
+
+def run_simple_benchmarks(
+    scan_percentages, output_prefix, num_threads, collect_latency
+):
     print("Running benchmarks matrix...")
     print(f" #threads: {num_threads}")
     for scan_percentage in sorted(scan_percentages):
@@ -36,24 +42,25 @@ def run_simple_benchmarks(scan_percentages, output_prefix, num_threads):
                 ]
                 print(f" Running:  scan {scan_percentage:3d}%  {protocol:7s}")
                 subprocess.run(
-                    [PROG_SIMPLE_BENCH] + options,
+                    [simple_bench_path(collect_latency)] + options,
                     check=True,
                     stderr=subprocess.STDOUT,
                     stdout=output_file,
                 )
 
 
-def parse_results(scan_percentages, output_prefix):
+def parse_results(scan_percentages, output_prefix, collect_latency):
     print("Parsing benchmark results...")
     results = {}
     for protocol in PROTOCOLS:
-        results[protocol] = []
+        results[protocol] = {}
 
     for scan_percentage in sorted(scan_percentages):
         for protocol in PROTOCOLS:
             result_filename = f"{output_prefix}-{protocol}-c{scan_percentage}.log"
             with open(result_filename, "r") as result_file:
                 abort_rates, throughputs = [], []
+                exec_times, lock_times, validate_times, commit_times = [], [], [], []
 
                 for line in result_file.readlines():
                     line = line.strip()
@@ -67,22 +74,70 @@ def parse_results(scan_percentages, output_prefix):
                         )
                         assert throughput > 0.0
                         throughputs.append(throughput)
+                    elif line.startswith("Exec time:"):
+                        exec_time = float(line[line.index(":") + 1 : line.index("μs")])
+                        exec_times.append(exec_time)
+                    elif line.startswith("Lock time:"):
+                        lock_time = float(line[line.index(":") + 1 : line.index("μs")])
+                        lock_times.append(lock_time)
+                    elif line.startswith("Validate time:"):
+                        validate_time = float(
+                            line[line.index(":") + 1 : line.index("μs")]
+                        )
+                        validate_times.append(validate_time)
+                    elif line.startswith("Commit time:"):
+                        commit_time = float(
+                            line[line.index(":") + 1 : line.index("μs")]
+                        )
+                        commit_times.append(commit_time)
+
                 assert len(abort_rates) == len(throughputs)
                 assert len(throughputs) > 0
+                if collect_latency:
+                    assert (
+                        len(exec_times)
+                        == len(lock_times)
+                        == len(validate_times)
+                        == len(commit_times)
+                        == len(throughputs)
+                    )
 
                 avg_abort_rate = sum(abort_rates) / len(abort_rates)
                 avg_throughput = sum(throughputs) / len(throughputs)
-                print(
-                    f" Result:  scan {scan_percentage:3d}%  {protocol:7s}"
-                    f"  abort {avg_abort_rate:4.1f}%  {avg_throughput:.2f} txns/sec"
-                )
+                avg_exec_time = sum(exec_times) / len(exec_times)
+                avg_lock_time = sum(lock_times) / len(lock_times)
+                avg_validate_time = sum(validate_times) / len(validate_times)
+                avg_commit_time = sum(commit_times) / len(commit_times)
 
-                results[protocol].append(avg_throughput)
+                if not collect_latency:
+                    print(
+                        f" Result:  scan {scan_percentage:3d}%  {protocol:7s}"
+                        f"  abort {avg_abort_rate:4.1f}%  {avg_throughput:.2f} txns/sec"
+                    )
+                    results[protocol][scan_percentage] = {
+                        "throughput": avg_throughput,
+                    }
+                else:
+                    print(
+                        f" Result:  scan {scan_percentage:3d}%  {protocol:7s}"
+                        f"  abort {avg_abort_rate:4.1f}%  {avg_throughput:.2f} txns/sec"
+                        f"  exec {avg_exec_time:8.2f} μs"
+                        f"  lock {avg_lock_time:8.4f} μs"
+                        f"  validate {avg_validate_time:8.4f} μs"
+                        f"  commit {avg_commit_time:8.4f} μs"
+                    )
+                    results[protocol][scan_percentage] = {
+                        "throughput": avg_throughput,
+                        "exec_time": avg_exec_time,
+                        "lock_time": avg_lock_time,
+                        "validate_time": avg_validate_time,
+                        "commit_time": avg_commit_time,
+                    }
 
     return results
 
 
-def plot_results(scan_percentages, results, output_prefix):
+def plot_results(scan_percentages, results, output_prefix, collect_latency):
     protocol_marker = {"silo": "o", "silo_hv": "v", "silo_nr": "x"}
     protocol_color = {"silo": "steelblue", "silo_hv": "orange", "silo_nr": "red"}
 
@@ -90,7 +145,10 @@ def plot_results(scan_percentages, results, output_prefix):
 
     for protocol in reversed(PROTOCOLS):
         xs = scan_percentages
-        ys = results[protocol]
+        ys = [
+            results[protocol][scan_percentage]["throughput"]
+            for scan_percentage in scan_percentages
+        ]
         assert len(xs) == len(ys)
 
         ys = list(map(lambda t: t / 1000.0, ys))
@@ -107,13 +165,67 @@ def plot_results(scan_percentages, results, output_prefix):
     plt.legend()
     plt.tight_layout()
 
-    plt.savefig(f"{output_prefix}-plot.png", dpi=200)
+    plt.savefig(f"{output_prefix}-throughput-plot.png", dpi=200)
+
+    if collect_latency:
+        for scan_percentage in scan_percentages:
+            labels = PROTOCOLS
+            exec_times = [
+                results[protocol][scan_percentage]["exec_time"]
+                for protocol in PROTOCOLS
+            ]
+            lock_times = [
+                results[protocol][scan_percentage]["lock_time"]
+                for protocol in PROTOCOLS
+            ]
+            validate_times = [
+                results[protocol][scan_percentage]["validate_time"]
+                for protocol in PROTOCOLS
+            ]
+            commit_times = [
+                results[protocol][scan_percentage]["commit_time"]
+                for protocol in PROTOCOLS
+            ]
+
+            plt.figure(scan_percentage)
+            plt.rcParams.update({"font.size": 18})
+
+            plt.barh(labels, exec_times, label="Exec", color="mediumseagreen")
+            left_lock = exec_times
+            plt.barh(
+                labels, lock_times, left=left_lock, label="Phase 1", color="orange"
+            )
+            left_validate = [
+                exec_times[i] + lock_times[i] for i in range(len(PROTOCOLS))
+            ]
+            plt.barh(
+                labels,
+                validate_times,
+                left=left_validate,
+                label="Phase 2",
+                color="steelblue",
+            )
+            left_commit = [
+                exec_times[i] + lock_times[i] + validate_times[i]
+                for i in range(len(PROTOCOLS))
+            ]
+            plt.barh(
+                labels, commit_times, left=left_commit, label="Phase 3", color="red"
+            )
+
+            # plt.xlim((0, 0.025))
+            plt.ylabel("Protocol")
+            plt.xlabel("Latency (μs)")
+            plt.legend()
+            plt.tight_layout()
+            plt.savefig(f"{output_prefix}-{scan_percentage}-latency-plot.png")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output_prefix", dest="output_prefix", required=True)
     parser.add_argument("-t", "--num_threads", dest="num_threads", default=16)
+    parser.add_argument("-l", "--latency", dest="collect_latency", action="store_true")
     parser.add_argument(
         "scan_percentages",
         metavar="C",
@@ -131,6 +243,15 @@ if __name__ == "__main__":
             print(f"Error: invalid scan percentage {scan_percentage}")
             exit(1)
 
-    run_simple_benchmarks(args.scan_percentages, args.output_prefix, args.num_threads)
-    results = parse_results(args.scan_percentages, args.output_prefix)
-    plot_results(args.scan_percentages, results, args.output_prefix)
+    run_simple_benchmarks(
+        args.scan_percentages,
+        args.output_prefix,
+        args.num_threads,
+        args.collect_latency,
+    )
+    results = parse_results(
+        args.scan_percentages, args.output_prefix, args.collect_latency
+    )
+    plot_results(
+        args.scan_percentages, results, args.output_prefix, args.collect_latency
+    )
